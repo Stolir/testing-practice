941e490ffc69a3feda0f2216049727b5d8893e16
Abdelrahman <119765066+Stolir@users.noreply.github.com>
2025-04-29 15:07:29 +0300

    Write getAverage helper function and pass relevant tests

--- analyzeArray/analyze.js
+++ analyzeArray/analyze.js
@@ -1,6 +1,14 @@
 export function analyzeArray(array) {
   let analyzed = {}
-
+  analyzed.average = getAverage(array);
   return analyzed;
+}
+
+function getAverage(array) {
+  let sum = 0;
+  for (let value of array) {
+    sum += +value;
+  }
+  return sum / array.length;
 }
 
